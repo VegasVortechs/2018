63230812e82941883d945a72d17cec646999740a
Jimson Huang <jimsonjy@gmail.com>
2018-01-14 01:37:37 -0800

    Initial Test Board

--- src/org/usfirst/frc/team6519/robot/Robot.java
+++ src/org/usfirst/frc/team6519/robot/Robot.java
@@ -1,10 +1,4 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
+// Version Test0.1
 package org.usfirst.frc.team6519.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
